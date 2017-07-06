c9920ff4de5f2a2231e831ebdbe664232439a9f5
Mohammad Abd <mohammad.abd.dk@gmail.com>
2017-07-06 20:33:14 +0200

    (making the Homework ...)

--- java.js
+++ java.js
@@ -1,3 +1,12 @@
  $(document).ready(function() {
-    $("#FirstID").text("testasdadasdas");
- };
+   $.getJSON("Movie_20.json", function(json) {
+        var html = "";
+        json.forEach(function(val) {
+  var keys = Object.keys(val);
+  html += "<div class = 'cat'>";
+  keys.forEach(function(key) {
+    $("#FirstID").push(key);
+  });
+  });
+});
+ });
